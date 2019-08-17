16d2f23610ee2f48f591bd89cdf9b8010b89c6ae
Nevil Alex <nevilneo010@gmail.com>
2019-08-17 13:56:35 +0530

    first commit

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20190817-080405";
+const char *build_timestamp = "2019-08-17T08:04:05Z";
+const char *build_version = "1.0";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20190817-080405/2.15.0-g787ac38";
+const char *mg_build_timestamp = "2019-08-17T08:04:05Z";
+const char *mg_build_version = "2.15.0";
